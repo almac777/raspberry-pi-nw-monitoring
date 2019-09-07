a296ea19fe712e422f36c5f3eaf4d08babed4f88
Alex Macovei <Alex.Macovei@twinformatics.at>
2019-09-07 17:09:20 +0200

    Added additional libraries
    - Added typed-inject
    - Added mocha and @types/mocha
    - Added test script
    - Added sample Unit Test

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,13 +2,13 @@
 import express = require('express');
 
 // Create a new express application instance
-const app: express.Application = express();
+const main: express.Application = express();
 
-app.get('/', function (req, res) {
+main.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+main.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
